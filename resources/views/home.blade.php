a69b75188546d74f3b050714b4c712a7d73b2138
NoeEsquivel <eesquivelc@miumg.edu.gt>
2023-03-04 05:33:12 -0600

    mi primer commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layaout')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    <p>Este es el texto de la vista secundaria.</p>
+@endsection
+
+@section('content')
+    <p>Este es el contenido de la vista</p>
+@endsection
